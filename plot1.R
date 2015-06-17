ac5280025e9ed64e4287b593d0f0ccf39e994b96
d-smith <doug.smith.mail@gmail.com>
2015-06-17 07:04:56 -0600

    Finished plot 1

--- plot1.R
+++ plot1.R
@@ -1,5 +1,13 @@
+## dplyr used to aggregate data by year
 library(dplyr)
+
+## Read data - assumes source data unzipped and present in current directory
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Aggregate total emissions by year, then plot.
 by_year <- group_by(NEI, year)
 em <- summarise(by_year, total_em=sum(Emissions))
-quartz()
-with(em, plot(total_em ~ year,type="l"))
+png(filename="./plot1.png", width=480, height=480)
+with(em, plot(total_em ~ year,type="o", ylab="Total Emissions (Tons)",xlab="Year"))
+dev.off()
