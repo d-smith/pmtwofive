6acc5cf952a51d500c6eb08c0cbd7ca43a645bb7
d-smith <doug.smith.mail@gmail.com>
2015-06-17 18:18:17 -0600

    added second plot

--- plot2.R
+++ plot2.R
@@ -6,9 +6,10 @@ NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 ## Aggregate total emissions by year, then plot.
-by_year <- group_by(NEI, year)
+baltimore <- filter(NEI,fips=="06037")
+by_year <- group_by(baltimore, year)
 em <- summarise(by_year, total_em=sum(Emissions))
-png(filename="./plot1.png", width=480, height=480)
+png(filename="./plot2.png", width=480, height=480)
 with(em, plot(total_em ~ year,type="o", ylab="Total Emissions (Tons)",
-              xlab="Year",main="United States Total Emissions By Year"))
+              xlab="Year",main="Baltimore Total Emission By Year"))
 dev.off()
